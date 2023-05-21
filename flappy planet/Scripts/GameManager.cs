6c3e8be49b306a9a8c6f6521e9410094551c1364
Nuno Miguel Areal Varela <131854498+NunoMiguelArealVarela@users.noreply.github.com>
2023-05-21 18:09:20 +0100

    Update GameManager.cs

--- flappy planet/Scripts/GameManager.cs
+++ flappy planet/Scripts/GameManager.cs
@@ -1,97 +1,75 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-
-    public Player player;
-    private Spawner spawner;
-    private Pipes planets;
-
-    public float initialGameSpeed = 5f;
-    public float gameSpeedIncrease = 0.1f;
-    public float gameSpeed { get; private set; }
-
-    public Text scoreText;
-    public Text hiscoreText;
-
-    public GameObject play;
+    public PlayerControl jogador;
+    public Text resultadoTexto;
+    public Text highscoreTexto;
     public GameObject gameOver;
-    public float score { get; private set; }
+    public GameObject playButton;
+    public float spawn; 
 
+    private int resultado;
 
-    private void Start(){
-        
-        
+    private void Start()
+    {
+        highscoreTexto.text = "HIGHSCORE: " + PlayerPrefs.GetInt("highscore", 0).ToString(); //vai buscar valor de highscore e vai dar print
+        //0 serve para dar valor default que so vai ser usado a primeira vez de todas que o jogador nao tem highscore
     }
 
-    private void Awake()
+    private void Awake() //quando jogo comeca quero pausa porque so quer comecar jogo quando carrego botao
     {
-    
-        Application.targetFrameRate = 60;
-
-        player = FindObjectOfType<Player>();
-        spawner = FindObjectOfType<Spawner>();
-        planets = FindObjectOfType<Pipes>();
-        Pause();
+        Application.targetFrameRate = 60; //nao obrigatorio mas referir com 60 frame por segundo
+        Pausar();
     }
 
-    public void Play()
+    public void Jogar()
     {
-        score = 0;
-        scoreText.text = score.ToString();
+        resultado = 0;
+        resultadoTexto.text = resultado.ToString();
 
-        play.SetActive(false);
+        playButton.SetActive(false);
         gameOver.SetActive(false);
 
-        Time.timeScale = 1f;
-        player.enabled = true;
+        Time.timeScale = 1;
+        jogador.enabled = true;
 
-        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        //quando clico play quero apagar os prefabs anteriores que estao na tela no instante que eu morri da ultima vez
+        Velocidade[] prefabs = FindObjectsOfType<Velocidade>();
 
-        for (int i = 0; i < pipes.Length; i++) {
-            Destroy(pipes[i].gameObject);
+        for (int i = 0; i < prefabs.Length; i++) {
+            Destroy(prefabs[i].gameObject);
         }
     }
 
-    public void GameOver()
+    public void Pausar()
     {
-        play.SetActive(true);
-        gameOver.SetActive(true);
+        Time.timeScale = 0f; //definir isto a zero e obrigar o jogador comecar parado porque tudo o que se mexe multiplica por tempo
+        jogador.enabled = false;
+    }
 
-        float hiscore = PlayerPrefs.GetFloat("hiscore", 0);
-        
 
-        if (score > hiscore)
+    public void JogoAcaba()
+    {
+        //quando ou bate no chao ou bate num pipe
+        if (resultado > PlayerPrefs.GetInt("highscore")) //entra aqui se resultado for maior que highscore
         {
-            hiscore = score;
-            PlayerPrefs.SetFloat("hiscore", hiscore);
+            PlayerPrefs.SetInt("highscore", resultado); //vai guardar na key highscore o valor de resultado
+            highscoreTexto.text = "HIGHSCORE: " + resultado; //e vai dar print
         }
 
-        hiscoreText.text = "HI: "+ Mathf.FloorToInt(hiscore).ToString();
-        Pause();
-        
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+
+        Pausar();
     }
 
-    public void Pause()
+    public void AumentaResultado()
     {
-        Time.timeScale = 0f;
-        player.enabled = false;
+        resultado++; //quando passa na score zona entre os planetas
+        resultadoTexto.text = resultado.ToString();
     }
-
-    public void IncreaseScore()
-    {
-        score++;
-        scoreText.text = score.ToString();
-    }
-
-
-    private void Update()
-    {
-        gameSpeed += gameSpeedIncrease * Time.deltaTime;
-        score += gameSpeed * Time.deltaTime;
-        scoreText.text = Mathf.FloorToInt(score).ToString();
-        
-    }
-
 }
